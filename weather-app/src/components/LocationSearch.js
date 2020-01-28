94755838adb5f444c6733a4c325b186cc359e6b6
Adam Mathieson <mathieson1222@gmail.com>
2020-01-27 22:44:20 -0800

    adds react-geocode and returns lat and lon by city (not via search input)

--- weather-app/src/components/LocationSearch.js
+++ weather-app/src/components/LocationSearch.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import WeatherInfo from './WeatherInfo';
+import geocode from 'react-geocode'
+
+geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY)
+geocode.setRegion("us")
+
+geocode.fromAddress("Seattle, WA").then(
+    res => {
+        const { lat, lng } = res.results[0].geometry.location
+        console.log(lat, lng)
+    }
+)
 
 const key = process.env.REACT_APP_DARK_SKY_API_KEY
+
 
 const LocationSearch = () => {
     const [location, setLocation] = useState({})
@@ -9,6 +21,11 @@ const LocationSearch = () => {
     return (
         <div className="search-container">
             <h2>Enter Location</h2>
+            <input 
+                type="text"
+                id="search-location"
+                placeholder="search for location"
+            />
             <WeatherInfo location={location}/>
         </div>
     );
