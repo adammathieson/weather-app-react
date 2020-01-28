390f4993a7e4731da2fbfbf81eb1bc58dd330710
Adam Mathieson <mathieson1222@gmail.com>
2020-01-28 14:07:38 -0800

    basic styles; adds a footer and href to darksky

---
+++ weather-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+        </div>
+    )
+}
+
+export default Footer
